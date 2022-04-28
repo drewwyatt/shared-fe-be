651a6568f119229ee42663183fcc5cca0a629048
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-04-28 00:00:25 -0400

    move front-end code into "frontend/"

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// create-react-app entrypoint
+//
+// It looks like CRA _has_ to look at src/index as its root.
+// I'd recommend putting a very thin file here with a comment (like this one)
+// so that all your front-end code can still live in "frontent/"
+import './frontend'
